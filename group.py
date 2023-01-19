826efcb6c26acce2a7155e87e80f8b5643e89990
isgibbs <isaac@fprime.com>
2023-01-19 13:32:18 -0800

    NewSims

--- group.py
+++ group.py
@@ -60,7 +60,7 @@ def _compute_adaptive_threshold(
     return threshold, var_dict
 
 def compute_group_coverages(
-    x_calib, scores_calib, scores_test, groups_test, x_test, alpha
+    x_calib, scores_calib, scores_test, groups_test, x_test, alpha, exact = False
 ):
     coverages = {i : [] for i in range(groups_test.shape[1])}
     coverages[-1] = []
@@ -68,10 +68,15 @@ def compute_group_coverages(
     prob = setup_cvx_problem(x_calib, scores_calib, alpha)
 
     for i in tqdm(range(len(x_test))):
-        threshold, _ = _compute_adaptive_threshold(
-            prob, x_test[i,:], np.max(scores_calib)
-        )
-        coverage = scores_test[i] < threshold
+        if not exact:
+            threshold, _ = _compute_adaptive_threshold(
+                prob, x_test[i,:], np.max(scores_calib)
+            )
+        else:
+            threshold, _ = _compute_adaptive_threshold(
+                prob, x_test[i,:], scores_test[i]
+            )
+        coverage = scores_test[i] <= threshold
         for gp in np.where(groups_test[i] > 0)[0]:
             coverages[gp].append(coverage)
         coverages[-1].append(coverage)
@@ -85,10 +90,83 @@ def compute_split_coverages(
     split_coverages = {i : [] for i in range(groups_test.shape[1])}
     split_coverages[-1] = []
     
-    for i in tqdm(range(len(groups_test))):
-        coverage = scores_test[i] < split_threshold
+    for i in (range(len(groups_test))):
+        coverage = scores_test[i] <= split_threshold
         for gp in np.where(groups_test[i] > 0)[0]:
             split_coverages[gp].append(coverage)
         split_coverages[-1].append(coverage)
 
     return {gp : np.mean(coverages) for gp, coverages in split_coverages.items()}
+
+def compute_qr_coverages(
+    groups_train, groups_test, scores_train, scores_test, alpha
+    
+):
+    prob = setup_cvx_problem(groups_train[0:(len(scores_train)-1),:], scores_train[0:(len(scores_train)-1)], alpha)
+    prob.param_dict["score_impute"].value = np.asarray([[scores_train[-1]]])
+    prob.param_dict["x_test"].value = groups_train[-1,:].reshape(1,groups_train[-1,:].shape[0])
+    prob.solve(solver='MOSEK', verbose=False)
+    
+    beta = prob.var_dict['params'].value
+    beta0 = prob.var_dict['intercept'].value
+
+    qr_coverages = {i : [] for i in range(groups_test.shape[1])}
+    qr_coverages[-1] = []
+
+    for i in (range(len(groups_test))):
+        coverage = scores_test[i] <= beta0 + beta@groups_test[i,:] 
+        for gp in np.where(groups_test[i] > 0)[0]:
+            qr_coverages[gp].append(coverage)
+        qr_coverages[-1].append(coverage)
+        
+    return {gp : np.mean(coverages) for gp, coverages in qr_coverages.items()}
+
+
+def compute_cqr_coverages(
+    groups_train, groups_calib, groups_test, scores_train, scores_calib, scores_test, alpha
+):
+    prob = setup_cvx_problem(groups_train[0:(len(scores_train)-1),:], scores_train[0:(len(scores_train)-1)], alpha)
+    prob.param_dict["score_impute"].value = np.asarray([[scores_train[-1]]])
+    prob.param_dict["x_test"].value = groups_train[-1,:].reshape(1,groups_train[-1,:].shape[0])
+    prob.solve(solver='MOSEK', verbose=False)
+    
+    beta = prob.var_dict['params'].value
+    beta0 = prob.var_dict['intercept'].value
+    
+    qPred = groups_calib@beta + beta0
+    scores = scores_calib-qPred
+    q = np.quantile(scores, [alpha * (1 + 1/len(scores_calib))])
+    
+    cqr_coverages = {i : [] for i in range(groups_test.shape[1])}
+    cqr_coverages[-1] = []
+
+    for i in (range(len(groups_test))):
+        coverage = scores_test[i] <= beta0 + beta@groups_test[i,:] + q
+        for gp in np.where(groups_test[i] > 0)[0]:
+            cqr_coverages[gp].append(coverage)
+        cqr_coverages[-1].append(coverage)
+    return {gp : np.mean(coverages) for gp, coverages in cqr_coverages.items()}
+
+# n = 1250
+# x_std = 0.1
+# y_std = 0.25
+# d = 15 # choose d > 10
+
+# # std_dev_list = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+# std_dev_list = np.array([5.0, 0.1, 5.0, 0.1, 0.1, 5.0, 0.1, 5.0, 5.0, 0.1]) 
+
+# theta = np.random.normal(loc=np.zeros(d), scale=x_std)
+# from Synthetic_data_generation import generate_group_synthetic_data, get_groups
+# x_train_final, y_train_final, x_calib, y_calib, x_test, y_test = generate_group_synthetic_data(
+#     n, x_std, y_std, d, std_dev_list, theta, 100, 100
+# )
+# groups_test = get_groups(x_test[:,0:10])
+# groups_calib = get_groups(x_calib[:,0:10])
+# groups_train = get_groups(x_train_final[:,0:10])
+# scores_train = y_train_final
+# scores_calib = y_calib
+# scores_test = y_test
+
+# compute_group_coverages(
+#         groups_train, y_train_final, y_test, groups_test, groups_test, 0.9, exact = False
+#     )
--- rkhs.py
+++ rkhs.py
@@ -5,6 +5,7 @@ import mosek
 from tqdm import tqdm
 
 from sklearn.metrics.pairwise import pairwise_kernels
+from sklearn.model_selection import KFold
 
 def setup_cvx_primal(
     x_calib, scores_calib, kernel, gamma, alpha, radius
@@ -36,12 +37,41 @@ def setup_cvx_primal(
     loss = cp.sum(0.5 * cp.abs(resid) + cp.multiply(alpha - 0.5, resid)) * (1/(n_calib + 1))
     loss += (1/(2*radius)) * cp.sum_squares(L.T @ rkhs_weight)
     prob = cp.Problem(cp.Minimize(loss))
+  
+    return prob
+
+def setup_full_cvx_dual(
+    x_calib, scores_calib, kernel, gamma, alpha
+):
+    n_calib = len(scores_calib)
+
+    rkhs_weight = cp.Variable(name="weights", shape=n_calib )
+
+    _, L = _get_kernel_matrix(x_calib, kernel, gamma)
+
+    scores_const = cp.Constant(scores_calib.reshape(-1,1))
+    
+    lamb = cp.Parameter(name="lambda", shape=(1,1))
+
+    L_const = cp.Constant(L)
+
+    C = lamb *  (n_calib)
+
+    constraints = [
+        C * (alpha - 1) <= rkhs_weight,
+        C * alpha >= rkhs_weight,
+        cp.sum(cp.multiply(rkhs_weight, np.ones(n_calib))) == 0]
+    prob = cp.Problem(
+        cp.Minimize(0.5 * cp.sum_squares(L_const.T @ rkhs_weight) -
+                    cp.sum(cp.multiply(rkhs_weight, cp.vec(scores_const)))),
+        constraints
+    )
 
     return prob
 
 
 def setup_cvx_dual(
-    x_calib, scores_calib, kernel, gamma, alpha, radius
+    x_calib, scores_calib, kernel, gamma, alpha
 ):
     n_calib = len(scores_calib)
 
@@ -52,6 +82,8 @@ def setup_cvx_dual(
     scores_const = cp.Constant(scores_calib.reshape(-1,1))
     scores_param = cp.Parameter(name="score_impute", shape=(1,1))
     scores = cp.vstack([scores_const, scores_param])
+    
+    lamb = cp.Parameter(name="lambda", shape=(1,1))
 
     L_11_const = cp.Constant(
         np.hstack([L_11, np.zeros((L_11.shape[0], 1))])
@@ -59,8 +91,7 @@ def setup_cvx_dual(
     L_21_22_param = cp.Parameter(name="L_21_22", shape=(1, n_calib + 1))
     L = cp.vstack([L_11_const, L_21_22_param])
 
-    l = 1/radius
-    C = 1 / (l * (n_calib + 1))
+    C = lamb *  (n_calib + 1)
 
     constraints = [
         C * (alpha - 1) <= rkhs_weight,
@@ -84,14 +115,15 @@ def _get_kernel_matrix(x_calib, kernel, gamma):
     K_chol = np.linalg.cholesky(K)
     return K, K_chol
 
-def compute_adaptive_threshold(
+def finish_dual_setup(
     prob,
     scores_calib : np.ndarray, 
     x_calib : np.ndarray,
     x_test : np.ndarray, 
     kernel : str, 
     gamma : float = 1,
-    M : float = None
+    M : float = None,
+    radius : float = 1
 ):
     if M is None:
         M = np.max(scores_calib)
@@ -120,6 +152,31 @@ def compute_adaptive_threshold(
     L_22[L_22 < 0] = 0
     L_22 = np.sqrt(L_22)
     prob.param_dict['L_21_22'].value = np.hstack([L_21, L_22])
+    
+    prob.param_dict['lambda'].value = np.asarray([[1/radius]])
+    
+    return prob
+
+def compute_adaptive_threshold(
+    prob,
+    scores_calib : np.ndarray, 
+    x_calib : np.ndarray,
+    x_test : np.ndarray, 
+    kernel : str, 
+    gamma : float = 1,
+    M : float = None,
+    radius : float = 1
+):
+    prob = finish_dual_setup(
+                prob,
+                scores_calib, 
+                x_calib,
+                x_test, 
+                kernel, 
+                gamma,
+                M,
+                radius
+            )
     prob.solve(
         solver='MOSEK', 
         verbose=False, 
@@ -140,25 +197,54 @@ def compute_shifted_coverage(
     scores_calib : np.ndarray, 
     x_calib : np.ndarray, 
     x_test : np.ndarray,
-    shift_weights : np.ndarray, 
+    shift_loc : float, 
     kernel : str, 
     alpha : float, 
     radius : float,
     gamma : float
 ):        
-    est_coverages = []
     obs_coverages = []
     thresholds = []
+    
+    
+    ### Get estimated Coverage
+    prob_calib = setup_cvx_dual(
+        x_calib[:-1,:],
+        scores_calib[:-1],
+        kernel=kernel,
+        gamma=gamma,
+        alpha=alpha
+    )
+    f_hat = compute_adaptive_threshold(
+        prob_calib,
+        scores_calib[:-1],
+        x_calib[:-1,:],
+        x_calib[-1,:],
+        kernel=kernel,
+        gamma=gamma,
+        radius=radius
+    )
 
+    g = pairwise_kernels(
+        X=np.array([shift_loc]).reshape(1,-1),
+        Y = x_calib,
+        metric=kernel,
+        gamma=gamma
+    )
+
+    inner_product = g @ f_hat['weights']
+    g_sum = np.mean((g))
+    est_coverage = alpha - (1 / radius) * (inner_product / g_sum)
+
+    ### Compute empircal coverage
     prob = setup_cvx_dual(
         x_calib,
         scores_calib,
         kernel=kernel,
         gamma=gamma,
-        alpha=alpha,
-        radius=radius
+        alpha=alpha
     )
-
+    
     for i in tqdm(range(len(x_test))):
         f_hat = compute_adaptive_threshold(
             prob,
@@ -166,19 +252,9 @@ def compute_shifted_coverage(
             x_calib,
             x_test[i,:],
             kernel=kernel,
-            gamma=gamma
+            gamma=gamma,
+            radius=radius
         )
-
-        K = pairwise_kernels(
-            X=np.concatenate([x_calib, x_test[i,:].reshape(1,-1)], axis=0),
-            metric=kernel,
-            gamma=gamma
-        )
-
-        inner_product = shift_weights.T @ K @ f_hat['weights']
-        g_sum = np.mean((K @ shift_weights)[:-1])
-        est_coverage = alpha - (1 / radius) * (inner_product / g_sum)
-        est_coverages.append(est_coverage)
 
         K_test = pairwise_kernels(
             X=x_test[i,:].reshape(1,-1),
@@ -191,10 +267,62 @@ def compute_shifted_coverage(
         coverage = int(scores_test[i] < score_threshold)
         obs_coverages.append(coverage)
 
-    K_g = pairwise_kernels(
-        X=x_test,
-        Y=np.concatenate([x_calib, x_test[0,:].reshape(1,-1)], axis=0),
+    g = pairwise_kernels(
+        X=np.array([shift_loc]).reshape(1,-1),
+        Y=x_test,
         metric=kernel,
         gamma=gamma
-    )
-    return np.mean(est_coverages), np.average(obs_coverages, weights=K_g @ shift_weights), np.asarray(thresholds).flatten()
+    )[0,:]
+    return est_coverage[0], np.average(obs_coverages, weights=g), np.asarray(thresholds).flatten()
+
+def runCV(x_calib,scores_calib,kernel,gamma,alpha,k,min_lamb = None,max_lamb=1, num_lamb = 20):
+    if min_lamb == None:
+        min_lamb = 1/len(scores_calib)
+    lambdas = np.linspace(min_lamb,max_lamb,num_lamb)
+        
+    folds = KFold(n_splits = k)
+    cvxProblemList = []
+    Klist = []
+    for i, (train_index, test_index) in enumerate(folds.split(x_calib)):
+        prob = setup_full_cvx_dual(
+                x_calib[train_index,:],
+                scores_calib[train_index],
+                kernel=kernel,
+                gamma=gamma,
+                alpha=alpha
+            )
+        cvxProblemList.append(prob)
+        Klist.append(pairwise_kernels(
+            X=x_calib[test_index,:],
+            Y=x_calib[train_index,:],
+            metric=kernel,
+            gamma=gamma
+        ))
+        
+        
+    allLosses = np.zeros(len(lambdas))
+    countR = 0
+    for lamb in tqdm(lambdas):
+        i=0
+        for i, (train_index, test_index) in enumerate(folds.split(x_calib)):        
+            cvxProblemList[i].param_dict['lambda'].value = np.asarray([[lamb]])
+            cvxProblemList[i].solve(
+                solver='MOSEK', 
+                verbose=False, 
+                mosek_params={mosek.iparam.intpnt_solve_form: mosek.solveform.dual}
+            )
+            intercept = cvxProblemList[i].constraints[2].dual_value
+            resid = (scores_calib[test_index] -
+                    Klist[i] @ cvxProblemList[i].var_dict['weights'].value -
+                    intercept)
+            loss = sum(0.5 * np.abs(resid) + (alpha - 0.5)*resid) * (1/(len(test_index)))
+    
+            allLosses[countR] = allLosses[countR] + loss/k
+            
+        countR = countR + 1
+        
+        
+    return allLosses, lambdas
+            
+
+        
