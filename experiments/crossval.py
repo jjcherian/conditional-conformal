59c74a1f74e115f34c58fe4e03e0bf91dd7d5eac
isgibbs <49995522+isgibbs@users.noreply.github.com>
2023-06-26 17:18:46 -0700

    Experiments updated to match new package api

---
+++ experiments/crossval.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cvxpy as cp
+from sklearn.metrics.pairwise import pairwise_kernels
+from sklearn.model_selection import KFold
+from conditionalconformal.condconf import setup_cvx_problem_calib
+
+def runCV(XCalib,scoresCalib,kernel,gamma,alpha,k,minRad,maxRad,numRad,phiCalib):    
+    radii = np.linspace(minRad,maxRad,numRad)
+        
+    folds = KFold(n_splits = k, shuffle = True)
+    Klist = []
+    for i, (trainIndex, testIndex) in enumerate(folds.split(XCalib)):
+        Klist.append(pairwise_kernels(
+            X=XCalib[testIndex,:],
+            Y=XCalib[trainIndex,:],
+            metric=kernel,
+            gamma=gamma
+        ))
+        
+        
+    allLosses = np.zeros(len(radii))
+    countR = 0
+    for radius in radii:
+        for i, (trainIndex, testIndex) in enumerate(folds.split(XCalib)):        
+            prob = setup_cvx_problem_calib(1-alpha,XCalib[trainIndex,:],scoresCalib[trainIndex], phiCalib[trainIndex,:],
+                                           {'kernel': 'rbf', 'gamma': gamma, 'lambda' : 1/radius})
+            if "MOSEK" in cp.installed_solvers():
+                prob.solve(solver="MOSEK")
+            else:
+                prob.solve()
+            resid = (scoresCalib[testIndex] -
+                    Klist[i] @ prob.var_dict['weights'].value -
+                    phiCalib[testIndex,:]@prob.constraints[2].dual_value)
+            loss = sum(0.5 * np.abs(resid) + (1 - alpha - 0.5)*resid) * (1/(len(testIndex)))
+    
+            allLosses[countR] = allLosses[countR] + loss/k
+            
+        countR = countR + 1
+        
+        
+    return allLosses, radii
+
---
+++ experiments/temperatureScaling.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  6 13:20:02 2023
+
+@author: isaacgibbs
+"""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.utils.data as tdata
+import torch.optim as optim    
+
+class myDataset(tdata.Dataset):
+    def __init__(self, X, labels):
+        self.labels = torch.tensor(labels)
+        self.X = torch.tensor(X)
+    def __len__(self):
+        return len(self.labels)
+    def __getitem__(self, idx):
+        label = self.labels[idx]
+        Xs = self.X[idx,:]
+        return Xs, label
+
+### Code for this function taken from https://github.com/aangelopoulos/conformal_classification
+### Uses temperature scaling to normalize the probabilities output by a neural network
+def torch_ts(X,Y, max_iters=1000, lr=0.1, epsilon=0.0001):
+    dat = myDataset(X,Y)
+
+    calib_loader = tdata.DataLoader(dat, batch_size=128, shuffle=True, pin_memory=True)
+    nll_criterion = nn.CrossEntropyLoss()
+
+    T = nn.Parameter(torch.Tensor([1.3]))
+
+    optimizer = optim.SGD([T], lr=lr)
+    for iter in range(max_iters):
+        T_old = T.item()
+        for x, targets in calib_loader:
+            optimizer.zero_grad()
+            x = x
+            x.requires_grad = True
+            out = x/T
+            loss = nll_criterion(out, targets.long())
+            loss.backward()
+            optimizer.step()
+        if abs(T_old - T.item()) < epsilon:
+            break
+    return T 
